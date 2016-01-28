bf2905a4119f52de2a160792c4c917c73d21cda0
medsouz <medsouz99@gmail.com>
2016-01-28 18:08:21 -0500

    Added more dynamic content
    Login/logout buttons, follow button on profile only shows if logged in, and fixed some navigation issues.

--- public/js/nav.js
+++ public/js/nav.js
@@ -3,13 +3,22 @@
 
 	After the user accesses the page for the first time in their session all future pages will be written to the content div to prevent the page from refreshing and breaking the music player.
 */
-function setContentURL(url) {
+function setContentURL(url, addToHistory) {
 	$.ajax({
 		url: url,
 		success: function(data) {
 			console.log("Switching to " + url);
+
+			if(addToHistory)
+				history.pushState(null, null, url);
+
 			$("#content").html(data);
 			componentHandler.upgradeElements($("#content")[0]);
+			// Hide the navbar if it is currently open
+			if($(".mdl-layout__drawer").hasClass("is-visible"))
+				$(".mdl-layout")[0].MaterialLayout.drawerToggleHandler_();
+			//Scroll to the top of the new page
+			$("#content").scrollTop(0);
 		}
 	});
 }
@@ -18,8 +27,7 @@ $(document).ready(function(){
 	$(document).on('click', "a[href^='/']", function(e) {
 		if(!$(this)[0].hasAttribute("noajax")) {
 			url = $(this).attr('href');
-			history.pushState(null, null, url);
-			setContentURL(url);
+			setContentURL(url, true);
 			e.preventDefault();
 		}
 	});
